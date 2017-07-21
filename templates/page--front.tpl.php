3e65d9868254efce49e1fffb8ff63d39c55f4605
rdrew <robdrewupei@gmail.com>
2017-07-21 13:09:48 -0300

    main structure set up

---
+++ templates/page--front.tpl.php
@@ -0,0 +1,103 @@
+<div class="l-page">
+<!--______________________topnav______________________-->
+  <header class="l-header" role="banner">
+
+    <div class="title-bar" data-responsive-toggle="example-animated-menu" data-hide-for="medium">
+      <button class="menu-icon" type="button" data-toggle></button>
+      <div class="title-bar-title">Menu</div>
+    </div>
+
+    <div class="top-bar" id="example-animated-menu" data-animate="hinge-in-from-top spin-out">
+      <div class="top-bar-left">
+<!-- logo -->
+<?php $theme_path = drupal_get_path('theme', variable_get('theme_default', NULL)); ?>
+    <a href="www.upei.ca">
+        <img class="site-logo" src="<?php print $theme_path;?>/dist/assets/img/logo.svg">
+    </a>
+<!-- end logo -->
+        <ul class="dropdown menu" data-dropdown-menu>
+          <li>
+            <a href="#">One</a>
+            <ul class="menu vertical">
+              <li><a href="#">One</a></li>
+              <li><a href="#">Two</a></li>
+              <li><a href="#">Three</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Two</a></li>
+          <li><a href="#">Three</a></li>
+        </ul>
+      </div>
+<div class="top-bar-right">
+
+<button type="button" class="button" data-toggle="offCanvas">Open Menu</button>
+</div>
+    </div>
+</header>
+<!--++++++++++++++tabs+++++++++++++++++++++ -->
+<div class="searchtabs__container">
+    <div class="searchtabs">
+<ul class="tabs" data-active-collapse="true" data-tabs id="collapsing-tabs">
+  <li class="tabs-title is-active"><a href="#panel1c" aria-selected="true">Tab 1</a></li>
+  <li class="tabs-title"><a href="#panel2c">Tab 2</a></li>
+  <li class="tabs-title"><a href="#panel3c">Tab 3</a></li>
+  <li class="tabs-title"><a href="#panel4c">Tab 4</a></li>
+</ul>
+
+<div class="tabs-content" data-tabs-content="collapsing-tabs">
+  <div class="tabs-panel is-active" id="panel1c">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+  </div>
+  <div class="tabs-panel" id="panel2c">
+    <p>Vivamus hendrerit arcu sed erat molestie vehicula. Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor. Suspendisse dictum feugiat nisl ut dapibus.</p>
+  </div>
+  <div class="tabs-panel" id="panel3c">
+    <img class="thumbnail" src="assets/img/generic/rectangle-3.jpg">
+  </div>
+  <div class="tabs-panel" id="panel4c">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+  </div>
+</div>
+    </div>
+</div>
+
+<!--______________________messages_____________________________-->
+      <div class="l-messages">
+          <?php print render($page['highlighted']); ?>
+          <?php print $messages; ?>
+      </div>
+<!--______________________content_____________________________-->
+
+  <div class="l-main">
+    <div class="l-content " role="main">
+    <div class="content">
+      <?php print $breadcrumb; ?>
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <h1><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php print render($tabs); ?>
+      <?php print render($page['help']); ?>
+      <?php if ($action_links): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <?php print render($page['content']); ?>
+      <?php print $feed_icons; ?>
+    </div>
+</div>
+
+    <?php print render($page['sidebar_first']); ?>
+    <?php print render($page['sidebar_second']); ?>
+  </div>
+<!--______________________sidebar-first_______________________-->
+<!--______________________sidebar-second______________________-->
+<!--______________________footer______________________________-->
+  <footer class="l-footer" role="contentinfo">
+    <?php print render($page['footer']); ?>
+    <?php print render($page['subfooter']); ?>
+  </footer>
+<!--______________________subfooter___________________________-->
+</div>
+    <?php print render($page['offcanvas']); ?>
---
+++ templates/region--offcanvas.tpl.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation to display a region.
+ *
+ * Available variables:
+ * - $content: The content for this region, typically blocks.
+ * - $classes: String of classes that can be used to style contextually through
+ *   CSS. It can be manipulated through the variable $classes_array from
+ *   preprocess functions. The default values can be one or more of the
+ *   following:
+ *   - region: The current template type, i.e., "theming hook".
+ *   - region-[name]: The name of the region with underscores replaced with
+ *     dashes. For example, the page_top region would have a region-page-top
+ *     class.
+ * - $region: The name of the region variable as defined in the theme's .info
+ *   file.
+ *
+ * Helper variables:
+ * - $classes_array: Array of html class attribute values. It is flattened
+ *   into a string within the variable $classes.
+ * - $is_admin: Flags true when the current user is an administrator.
+ * - $is_front: Flags true when presented in the front page.
+ * - $logged_in: Flags true when the current user is a logged-in member.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_region()
+ * @see template_process()
+ */
+?>
+<div class="off-canvas offcanvas position-right" id="offCanvas" data-off-canvas>
+
+    <!-- Close button -->
+    <button class="close-button" aria-label="Close menu" type="button" data-close>
+      <span aria-hidden="true">&times;</span>
+    </button>
+<?php print $content; ?>
+  </div>
+
+  <div class="off-canvas-content" data-off-canvas-content>
+    <!-- Your page content lives here -->
+  </div>
