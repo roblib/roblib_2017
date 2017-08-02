197c7f6ab0a473762bbba1d807da4ae6745d26e3
rdrew <robdrewupei@gmail.com>
2017-08-02 15:45:12 -0300

    added menu css

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -63,7 +63,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 39);
+/******/ 	return __webpack_require__(__webpack_require__.s = 40);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -14482,10 +14482,11 @@ var _foundationSites = __webpack_require__(21);
 
 var _foundationSites2 = _interopRequireDefault(_foundationSites);
 
+__webpack_require__(39);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 window.$ = _jquery2.default;
-
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 //import './lib/foundation-explicit-pieces';
@@ -22240,6 +22241,98 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
 /***/ }),
 /* 39 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+(function ($) {
+
+  /**
+   * The recommended way for producing HTML markup through JavaScript is to write
+   * theming functions. These are similiar to the theming functions that you might
+   * know from 'phptemplate' (the default PHP templating engine used by most
+   * Drupal themes including Omega). JavaScript theme functions accept arguments
+   * and can be overriden by sub-themes.
+   *
+   * In most cases, there is no good reason to NOT wrap your markup producing
+   * JavaScript in a theme function.
+   */
+  Drupal.theme.prototype.roblib2017ExampleButton = function (path, title) {
+    // Create an anchor element with jQuery.
+    return $('<a href="' + path + '" title="' + title + '">' + title + '</a>');
+  };
+
+  /**
+   * Behaviors are Drupal's way of applying JavaScript to a page. In short, the
+   * advantage of Behaviors over a simple 'document.ready()' lies in how it
+   * interacts with content loaded through Ajax. Opposed to the
+   * 'document.ready()' event which is only fired once when the page is
+   * initially loaded, behaviors get re-executed whenever something is added to
+   * the page through Ajax.
+   *
+   * You can attach as many behaviors as you wish. In fact, instead of overloading
+   * a single behavior with multiple, completely unrelated tasks you should create
+   * a separate behavior for every separate task.
+   *
+   * In most cases, there is no good reason to NOT wrap your JavaScript code in a
+   * behavior.
+   *
+   * @param context
+   *   The context for which the behavior is being executed. This is either the
+   *   full page or a piece of HTML that was just added through Ajax.
+   * @param settings
+   *   An array of settings (added through drupal_add_js()). Instead of accessing
+   *   Drupal.settings directly you should use this because of potential
+   *   modifications made by the Ajax callback that also produced 'context'.
+   */
+  Drupal.behaviors.roblib2017ExampleBehavior = {
+    attach: function attach(context, settings) {
+      // By using the 'context' variable we make sure that our code only runs on
+      // the relevant HTML. Furthermore, by using jQuery.once() we make sure that
+      // we don't run the same piece of code for an HTML snippet that we already
+      // processed previously. By using .once('foo') all processed elements will
+      // get tagged with a 'foo-processed' class, causing all future invocations
+      // of this behavior to ignore them.
+      $('.some-selector', context).once('foo', function () {
+        // Now, we are invoking the previously declared theme function using two
+        // settings as arguments.
+        var $anchor = Drupal.theme('roblib2017ExampleButton', settings.myExampleLinkPath, settings.myExampleLinkTitle);
+
+        // The anchor is then appended to the current element.
+        $anchor.appendTo(this);
+      });
+    }
+  };
+
+  //Drupal.behaviors.searchtabs = {
+  //attach: function (context, settings) {
+
+  //alert("yay");
+  ////$('.accordion-tabs').hide();
+  //$('.accordion-tabs').each(function(index) {
+  //$(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
+  //});
+  //$('.accordion-tabs').on('click', 'li > a.tab-link', function(event) {
+  //if (!$(this).hasClass('is-active')) {
+  //event.preventDefault();
+  //var accordionTabs = $(this).closest('.accordion-tabs');
+  //accordionTabs.find('.is-open').removeClass('is-open').hide();
+
+  //$(this).next().toggleClass('is-open').toggle();
+  //accordionTabs.find('.is-active').removeClass('is-active');
+  //$(this).addClass('is-active');
+  //} else {
+  //event.preventDefault();
+  //}
+  //});
+
+  //}
+  //};
+})(jQuery);
+
+/***/ }),
+/* 40 */
 /***/ (function(module, exports, __webpack_require__) {
 
 module.exports = __webpack_require__(20);
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -7,6 +7,6 @@ import Foundation from 'foundation-sites';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 //import './lib/foundation-explicit-pieces';
-
+import './roblib-2017.behaviors.js';
 
 $(document).foundation();
