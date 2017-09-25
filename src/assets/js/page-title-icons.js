7b08ca4c60e7d18cafd969080e43a216ac1454e9
rdrew <robdrewupei@gmail.com>
2017-09-25 12:49:54 -0300

    .

---
+++ src/assets/js/page-title-icons.js
@@ -0,0 +1,44 @@
+(function ($) {
+
+    alert("yay, it works");
+    //Drupal.behaviors.pageIcons = {
+        //attach: function (context, settings) {
+
+
+            //var pageTitle = $(".page-databases-all .page_title"); //select the element
+            //var icon = 'fa-search';
+            //pageTitle.prepend('<i class="fa ' + icon + '"></i>');
+            ////==============================================
+            //var pageTitle = $(".section-bar-askus .page_title"); //select the element
+            //var icon = 'fa-weixin';
+            //pageTitle.prepend('<i class="fa ' + icon + '"></i>');
+            ////==============================================
+            //var pageTitle = $(".section-computers .page_title"); //select the element
+            //var icon = 'fa-laptop';
+            //pageTitle.prepend('<i class="fa ' + icon + '"></i>');
+            ////==============================================
+            //var pageTitle = $(".section-printing .page_title"); //select the element
+            //var icon = 'fa-print';
+            //pageTitle.prepend('<i class="fa ' + icon + '"></i>');
+            ////==============================================
+            //var pageTitle = $(".section-peicollection .page_title"); //select the element
+            //var icon = 'fa-picture-o';
+            //pageTitle.prepend('<i class="fa ' + icon + '"></i>');
+            ////==============================================
+            //var pageTitle = $(".section-tours .page_title"); //select the element
+            //var icon = 'fa-info-circle';
+            //pageTitle.prepend('<i class="fa ' + icon + '"></i>');
+            ////==============================================
+            //var pageTitle = $(".section-sg .page_title"); //select the element
+            //var icon = 'fa-book';
+            //pageTitle.prepend('<i class="fa ' + icon + '"></i>');
+            ////==============================================
+            //var pageTitle = $(".section-faq-frequently-asked-questions-about-library-resources-and-services .page_title"); //select the element
+            //var icon = 'fa-question-circle';
+            //pageTitle.prepend('<i class="fa ' + icon + '"></i>');
+
+        //}
+    //};
+
+
+})(jQuery);
