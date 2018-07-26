1ccf6f6d3d915b32d04bd2cd1a34170e4069e27c
rdrew <rdrew@upei.ca>
2018-07-26 13:35:10 -0300

    .

--- template.php
+++ template.php
@@ -14,5 +14,24 @@ function roblib_2017_field__field_proxied_link__database($variables) {
 function roblib_2017_css_alter(&$css) {
  // unset($css[drupal_get_path('module', 'room_reservations') . '/room_reservations.css']);
   unset($css[drupal_get_path('theme', 'omega') . '/omega/css/modules/system/system.messages.theme.css']);
+  unset($css[drupal_get_path('module', 'date') . '/date/date_popup/themes/datepicker.1.7.css ']);
+
 }
+
+
+drupal_add_js(drupal_get_path('theme', 'roblib_2017') . '/dist/assets/js/app.js', array(
+  'preprocess' => FALSE,
+  'group' => JS_THEME,
+  'every_page' => TRUE,
+  'weight' => 1000,
+  )
+);
+drupal_add_css(drupal_get_path('theme', 'roblib_2017') . '/dist/assets/css/app.css', array(
+  'preprocess' => FALSE,
+  'group' => CSS_THEME,
+  'media' => 'screen',
+  'every_page' => TRUE,
+  'weight' => 1000,
+  )
+);
 
---
+++ templates/events/views-view-fields---rd-tabbed-event-list.tpl.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to all the fields as a row.
+ *
+ * - $view: The view in use.
+ * - $fields: an array of $field objects. Each one contains:
+ *   - $field->content: The output of the field.
+ *   - $field->raw: The raw data for the field, if it exists. This is NOT output safe.
+ *   - $field->class: The safe class id to use.
+ *   - $field->handler: The Views field handler object controlling this field. Do not use
+ *     var_export to dump this object, as it can't handle the recursion.
+ *   - $field->inline: Whether or not the field should be inline.
+ *   - $field->inline_html: either div or span based on the above flag.
+ *   - $field->wrapper_prefix: A complete wrapper containing the inline_html to use.
+ *   - $field->wrapper_suffix: The closing tag for the wrapper.
+ *   - $field->separator: an optional separator that may appear before a field.
+ *   - $field->label: The wrap label text to use.
+ *   - $field->label_html: The full HTML of the label to use including
+ *     configured element type.
+ * - $row: The raw result object from the query, with all data it fetched.
+ *
+ * @ingroup views_templates
+ */
+?>
+
+<div class="event_info_left">
+	<div class="event_date_wrapper">
+		<span class="event_date_month">
+			<?php print $fields['field_event_date_1']->content; ?>
+		</span>
+		<span class="event_date_dom">
+			<?php print $fields['field_event_date_2']->content; ?>
+		</span>
+		<span class="event_date_dow">
+			<?php print $fields['field_event_date_3']->content; ?>
+		</span>
+	</div>
+</div>
+<div class="event_info_right">
+		<h3 class="event_title">
+			<?php print $fields['title']->content; ?>
+		</h3>
+	<div class="event_thumbnail">
+		<?php print $fields['field_image']->content; ?>
+	</div>
+	<div class="event_details">
+		<div class="event_date_long">
+			<?php print $fields['field_event_date']->content; ?>
+		</div>
+		<div class="event_category">
+			<?php print $fields['field_event_type']->content; ?>
+		</div>
+	</div>
+</div>
